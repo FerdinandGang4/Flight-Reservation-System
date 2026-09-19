38984e503d496d41f148d6391c6c4c8a2b8e4761
Gang Ferdinand Dinga <dingagang@yahoo.com>
2026-09-19 14:50:39 -0500

    Creating passenger microservice startup project with all dependencies

---
+++ src/Services/PasengerMicroservice/PassengerMicroservice/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassengerMicroservice.Model;
+
+namespace PassengerMicroservice.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Pessenger> Passengers { get; set; }
+    }
+}
