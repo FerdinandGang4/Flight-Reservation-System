843fd85c33b01eece86584f0cccdf91d2f11fd0c
Gang Ferdinand Dinga <dingagang@yahoo.com>
2026-09-19 16:55:37 -0500

    cleanup program.cs file to remove weather infor

--- src/Services/BookingMicroservice/BookingMicroservervice/Program.cs
+++ src/Services/BookingMicroservice/BookingMicroservervice/Program.cs
@@ -7,7 +7,7 @@ namespace BookingMicroservervice
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddAuthorization();
+
 
 
             var app = builder.Build();
@@ -16,25 +16,7 @@ namespace BookingMicroservervice
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
-
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            });
+            
 
             app.Run();
         }
